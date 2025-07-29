3c8dea11a61c08d9be111faae1620d4f42cf497c
Prakash Raut <prakash_raut@outlook.com>
2025-07-29 16:25:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function toCase(text) {
   // write your code here
+  if (text.length === 0) return "-";
+  
+  const lower = text.toLowerCase();
+  const upper = text.toUpperCase();
+  
+  return `${lower}-${upper}`;
 }
 
 // DO not change the code below
